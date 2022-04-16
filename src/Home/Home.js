dc416123747445fdeab6e6f45558acb3b19dafb9
Xinran Gao <sherrygao821@hotmail.com>
2022-04-16 13:55:36 -0700

    set up router

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomeNavBar from './HomeNavBar';
 
 function Home() {
     return(
         <div>
-            
+            <HomeNavBar />
         </div>
     );
 }
