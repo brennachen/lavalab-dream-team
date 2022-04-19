cba260f1817425112b5831acceeee0c06cdd3a1b
brennajc <brennajc@usc.edu>
2022-04-18 18:37:45 -0700

    merge conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Home/Home";
-<<<<<<< HEAD
 import LogIn from "./SignUp/LogIn"
 import GeneralSignUp from './SignUp/GeneralSignUp';
 import StudentSignUp from './SignUp/StudentSignUp';
-=======
 import StudentHome from "./StudentHome/StudentHome";
->>>>>>> d932d0a07a9c2a330f72b566612cb434b1594fce
 
 function App() {
   return (
